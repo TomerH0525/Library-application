6e02168721598eaeb61132d2f1379ff2f5a5ed44
TomerH <tomer2b0525@gmail.com>
2024-07-06 22:41:53 +0300

    {start} initial commit

---
+++ src/test/java/com/tomer2h/LibraryApplication/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomer2h.LibraryApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
